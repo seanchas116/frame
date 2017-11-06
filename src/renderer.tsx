6a06a8a01b5c10fedfaa02ce217846ce171fe4e9
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-06 20:29:37 +0900

    Add App component

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { App } from './ui/App'
 
-ReactDOM.render(<div>Hello</div>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
