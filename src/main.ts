4400325a46b25a5fd91456c30c6549374dc4c782
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-12 15:53:27 +0900

    Run testapp

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { app, BrowserWindow, ipcMain } from 'electron'
 import * as qs from 'querystring'
 
-const dev = process.env.NODE_ENV === 'development'
-const contentBase = dev ? 'http://localhost:23000' : `file://${app.getAppPath()}/dist`
+const argv = require('minimist')(process.argv.slice(2))
+const contentBase = argv.development ? 'http://localhost:23000' : `file://${app.getAppPath()}/dist`
 
 let mainWindow: BrowserWindow | undefined
+let testWindow: BrowserWindow | undefined
 
 async function openWindow (filePath?: string) {
   const win = mainWindow = new BrowserWindow({
@@ -16,7 +17,7 @@ async function openWindow (filePath?: string) {
   const query = qs.stringify({ filePath })
 
   win.loadURL(`${contentBase}/index.html#${query}`)
-  if (dev) {
+  if (argv.development) {
     win.webContents.openDevTools()
   }
 
@@ -29,11 +30,34 @@ async function openWindow (filePath?: string) {
   })
 }
 
+function openTestWindow () {
+  const win = testWindow = new BrowserWindow({
+    width: 1200,
+    height: 768
+  })
+  win.loadURL(`${contentBase}/test.html`)
+  win.webContents.openDevTools()
+  win.on('closed', () => {
+    testWindow = undefined
+  })
+  ipcMain.on('testDone', (e, failCount) => {
+    if (!argv.devserver) {
+      setImmediate(() => {
+        process.exit(failCount)
+      })
+    }
+  })
+}
+
 app.commandLine.appendSwitch('enable-experimental-web-platform-features')
 
 app.on('ready', async () => {
-  ipcMain.on('newWindow', async (e: Electron.IpcMessageEvent, filePath?: string) => {
-    await openWindow(filePath)
-  })
-  await openWindow()
+  if (argv.test) {
+    await openTestWindow()
+  } else {
+    ipcMain.on('newWindow', async (e: Electron.IpcMessageEvent, filePath?: string) => {
+      await openWindow(filePath)
+    })
+    await openWindow()
+  }
 })
