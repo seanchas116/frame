5f9bce463ac290b78257bbef177c5b8eba2a71da
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-18 16:47:26 +0900

    Use normal mobx import

--- src/core/document/Document.ts
+++ src/core/document/Document.ts
@@ -1,5 +1,5 @@
 import { GroupLayer } from './Layer'
-import { observable } from 'mobx/lib/api/observable'
+import { observable } from 'mobx'
 
 export class Document {
   @observable rootGroup = new GroupLayer()
