43bedd2a7ece0879a39665251fe7adfea25620d8
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-18 16:48:05 +0900

    Aff File#toData test

--- src/test/file/v1/FileTest.ts
+++ src/test/file/v1/FileTest.ts
@@ -1,3 +1,15 @@
+import { assert } from 'chai'
+import { File } from '../../../core/file/v1/File'
+import { createDocument } from '../../document/DocumentFixture'
 
 describe('File', () => {
+  describe('#toData', () => {
+    it('serializes file to data', async () => {
+      const document = createDocument()
+      const file = new File(document)
+      const data = file.toData()
+      const file2 = await File.fromData(data)
+      assert.deepEqual(document, file2.document)
+    })
+  })
 })
