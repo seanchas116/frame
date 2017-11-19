6c1eb45486b53ccb03a1160b9ea4b9be8ce85b55
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-19 22:52:40 +0900

    Emit coverage json

--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,12 @@
 const requires = require.context('./', true, /Test\.ts$/)
 requires.keys().forEach(requires)
+
+after(() => {
+  const coverage = (window as any).__coverage__
+  if (coverage) {
+    console.log('Found coverage report, writing to coverage.json')
+    const path = require('path')
+    const fs = require('fs')
+    fs.writeFileSync(path.resolve(process.cwd(), 'coverage.json'), JSON.stringify(coverage))
+  }
+})
