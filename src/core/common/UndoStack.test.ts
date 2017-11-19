5fa54ba7817e6088cd432a07e232cc800a7560e2
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-20 00:43:02 +0900

    Add more UndoStack tests

--- src/core/common/UndoStack.test.ts
+++ src/core/common/UndoStack.test.ts
@@ -2,20 +2,49 @@ import { assert } from 'chai'
 import { UndoStack, UndoCommand } from './UndoStack'
 
 class ExampleCommand implements UndoCommand {
+  static undoneCommand = ''
+  static redoneCommand = ''
+
   constructor (public title: string) {}
 
   undo () {
-    // TODO
+    ExampleCommand.undoneCommand = this.title
   }
   redo () {
-    // TODO
+    ExampleCommand.redoneCommand = this.title
   }
+
 }
 
 describe('UndoStack', () => {
   let undoStack: UndoStack<ExampleCommand>
   beforeEach(() => {
     undoStack = new UndoStack()
+  })
+
+  describe('#commandToUndo', () => {
+    it('returns command to be undone', () => {
+      undoStack.push(new ExampleCommand('foo'))
+      undoStack.push(new ExampleCommand('bar'))
+      assert.equal(undoStack.commandToUndo!.title, 'bar')
+      undoStack.undo()
+      undoStack.undo()
+      assert.equal(undoStack.commandToUndo, undefined)
+    })
+  })
+
+  describe('#commandToRedo', () => {
+    it('returns command to be redone', () => {
+      assert.equal(undoStack.commandToRedo, undefined)
+      undoStack.push(new ExampleCommand('foo'))
+      undoStack.push(new ExampleCommand('bar'))
+      assert.equal(undoStack.commandToRedo, undefined)
+      undoStack.undo()
+      undoStack.undo()
+      assert.equal(undoStack.commandToRedo!.title, 'foo')
+      undoStack.push(new ExampleCommand('hoge'))
+      assert.equal(undoStack.commandToRedo, undefined)
+    })
   })
 
   describe('#canUndo', () => {
@@ -40,6 +69,39 @@ describe('UndoStack', () => {
       assert(undoStack.canRedo)
       undoStack.push(new ExampleCommand('hoge'))
       assert(!undoStack.canRedo)
+    })
+  })
+
+  describe('#undo', () => {
+    it('undos last command', () => {
+      undoStack.push(new ExampleCommand('foo'))
+      undoStack.push(new ExampleCommand('bar'))
+      undoStack.undo()
+      assert.equal(ExampleCommand.undoneCommand, 'bar')
+      undoStack.undo()
+      assert.equal(ExampleCommand.undoneCommand, 'foo')
+      undoStack.redo()
+      undoStack.redo()
+      undoStack.undo()
+      undoStack.redo()
+      assert.equal(ExampleCommand.undoneCommand, 'bar')
+    })
+  })
+
+  describe('#redo', () => {
+    it('redos last undone command', () => {
+      undoStack.push(new ExampleCommand('foo'))
+      undoStack.push(new ExampleCommand('bar'))
+      undoStack.undo()
+      undoStack.undo()
+      undoStack.redo()
+      assert.equal(ExampleCommand.redoneCommand, 'foo')
+      undoStack.redo()
+      assert.equal(ExampleCommand.redoneCommand, 'bar')
+      undoStack.push(new ExampleCommand('hoge'))
+      undoStack.undo()
+      undoStack.redo()
+      assert.equal(ExampleCommand.redoneCommand, 'hoge')
     })
   })
 })
