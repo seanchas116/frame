ee9bf36f65778946e2fcea711af1ca69ea326f23
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-25 14:25:37 +0900

    Add InsertOverlay

---
+++ src/ui/drawarea/InsertOverlay.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+const styles = require('./InsertOverlay.css')
+
+export class InsertOverlay extends React.Component {
+  render () {
+    return <div className={styles.InsertOverlay}>
+    </div>
+  }
+}
