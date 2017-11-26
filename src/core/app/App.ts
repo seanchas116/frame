9dafaba0c5fcc301e544ea22d7690e9176d2fd5f
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-26 17:43:49 +0900

    Add app insertMode

--- src/core/app/App.ts
+++ src/core/app/App.ts
@@ -1,7 +1,9 @@
 import { observable } from 'mobx'
 import { File } from './File'
 import { Document } from '../document/Document'
+import { ShapeType } from '../document/Shape'
 
 export class App {
   @observable file = new File(new Document())
+  @observable insertMode: ShapeType | undefined = undefined
 }
