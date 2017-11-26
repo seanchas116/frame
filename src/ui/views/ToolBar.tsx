4ae4e7bbf0035f2eee416d349d3b7eb4c7b8b660
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-26 20:27:39 +0900

    Style ToolBar

--- src/ui/views/ToolBar.tsx
+++ src/ui/views/ToolBar.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react'
+import { observer } from 'mobx-react'
+import * as classNames from 'classnames'
+import { ShapeType } from '../../core/document/Shape'
+import { app } from '../../core/app/App'
 const styles = require('./ToolBar.css')
 
-export class ToolBar extends React.Component {
+const ShapeToolIcon = observer((props: { type: ShapeType, className: string }) => {
+  const onClick = () => {
+    app.insertMode = props.type
+  }
+  const className = classNames(styles.ShapeToolIcon, props.className, {
+    [styles.ShapeToolIconSelected]: props.type === app.insertMode
+  })
+  return <div className={className} onClick={onClick}></div>
+})
 
+export class ToolBar extends React.Component {
   render () {
     return <div className={styles.ToolBar}>
+      <ShapeToolIcon type='rect' className={styles.ShapeToolIconRect} />
+      <ShapeToolIcon type='ellipse' className={styles.ShapeToolIconEllipse} />
+      <ShapeToolIcon type='text' className={styles.ShapeToolIconText} />
     </div>
   }
 }
