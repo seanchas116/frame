e38221bae8062edcc43f2ecc369f60a7a451b094
Ryohei Ikegami <iofg2100@gmail.com>
2017-11-26 18:15:38 +0900

    Add ToolBar

--- src/ui/views/App.tsx
+++ src/ui/views/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import { DrawArea } from '../drawarea/DrawArea'
+import { ToolBar } from './ToolBar'
 const styles = require('./App.css')
 
 export class App extends React.Component {
 
   render () {
     return <div className={styles.App}>
+      <ToolBar />
       <DrawArea />
     </div>
   }
---
+++ src/ui/views/ToolBar.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+const styles = require('./ToolBar.css')
+
+export class ToolBar extends React.Component {
+
+  render () {
+    return <div className={styles.ToolBar}>
+    </div>
+  }
+}
