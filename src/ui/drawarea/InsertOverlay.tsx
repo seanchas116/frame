bc8a723823d92e8f2cc2832e8b76d5297b00bbe0
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-01 22:11:02 +0900

    Use PointerEvents

--- src/ui/drawarea/InsertOverlay.tsx
+++ src/ui/drawarea/InsertOverlay.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
+import { Vec2 } from 'paintvec'
+import { PointerEvents } from '../common/PointerEvents'
 const styles = require('./InsertOverlay.css')
 
-export class InsertOverlay extends React.Component {
+export class InsertOverlay extends React.Component<{size: Vec2}> {
   render () {
-    return <div className={styles.InsertOverlay}>
-    </div>
+    const { width, height } = this.props.size
+    const style = {
+      width: `${width}px`,
+      height: `${height}px`
+    }
+    return <PointerEvents>
+      return <div className={styles.InsertOverlay} style={style} />
+    </PointerEvents>
   }
 }
