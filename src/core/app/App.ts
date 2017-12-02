66b2c764d1bc65e98f3677458982d8ec7a89f38a
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-02 17:37:42 +0900

    Add transforms

--- src/core/app/App.ts
+++ src/core/app/App.ts
@@ -1,4 +1,5 @@
-import { observable } from 'mobx'
+import { observable, computed } from 'mobx'
+import { Vec2, Transform } from 'paintvec'
 import { File } from './File'
 import { Document } from '../document/Document'
 import { ShapeType } from '../document/Shape'
@@ -6,6 +7,15 @@ import { ShapeType } from '../document/Shape'
 export class App {
   @observable file = new File(new Document())
   @observable insertMode: ShapeType | undefined = undefined
+
+  @observable scroll = new Vec2()
+
+  @computed get transformViewportToDocument () {
+    return Transform.translate(this.scroll)
+  }
+  @computed get transformDocumentToViewport () {
+    return Transform.translate(this.scroll.neg())
+  }
 }
 
 export const app = new App()
