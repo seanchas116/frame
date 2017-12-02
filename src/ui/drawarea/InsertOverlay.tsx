23079c95392cf99cc162be2ba62d8ba829a892df
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-02 15:38:30 +0900

    Add event handlers

--- src/ui/drawarea/InsertOverlay.tsx
+++ src/ui/drawarea/InsertOverlay.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Vec2 } from 'paintvec'
+import { action } from 'mobx'
 import { PointerEvents } from '../common/PointerEvents'
 const styles = require('./InsertOverlay.css')
 
@@ -10,8 +11,20 @@ export class InsertOverlay extends React.Component<{size: Vec2}> {
       width: `${width}px`,
       height: `${height}px`
     }
-    return <PointerEvents>
+    return <PointerEvents onPointerDown={this.handlePointerDown} onPointerMove={this.handlePointerMove} onPointerUp={this.handlePointerUp}>
       return <div className={styles.InsertOverlay} style={style} />
     </PointerEvents>
+  }
+
+  @action private handlePointerDown = (event: PointerEvent) => {
+    (event.currentTarget as Element).setPointerCapture(event.pointerId)
+  }
+
+  @action private handlePointerMove = (event: PointerEvent) => {
+    // TODO
+  }
+
+  @action private handlePointerUp = (event: PointerEvent) => {
+    // TODO
   }
 }
