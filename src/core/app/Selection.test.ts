98a30961ed6882b2aedb7fcf89c4c5df9efa19df
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-03 16:15:47 +0900

    Add Selection test

---
+++ src/core/app/Selection.test.ts
@@ -0,0 +1,26 @@
+import { assert } from 'chai'
+import { App } from './App'
+import { Layer } from '../document/Layer'
+
+describe('Selection', () => {
+  let app: App
+  beforeEach(() => {
+    app = new App()
+  })
+  describe('layers', () => {
+    it('returns selected layers sorted', () => {
+      const layer1 = new Layer()
+      const layer2 = new Layer()
+      const layer3 = new Layer()
+      const leakedLayer = new Layer()
+      layer2.children.push(layer3)
+      app.file.document.rootGroup.children.push(layer1, layer2)
+
+      app.selection.add(layer3)
+      app.selection.add(layer1)
+      app.selection.add(leakedLayer)
+
+      assert.deepEqual(app.selection.layers, [layer1, layer3])
+    })
+  })
+})
