bd68c243f7f575fd0a1f54cb4540476ea85e31b2
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-09 17:29:58 +0900

    Add test layer

--- src/core/app/App.ts
+++ src/core/app/App.ts
@@ -1,9 +1,11 @@
 import { observable } from 'mobx'
 import { File } from './File'
 import { Document } from '../document/Document'
-import { ShapeType } from '../document/Shape'
+import { ShapeType, RectShape } from '../document/Shape'
 import { Scroll } from './Scroll'
 import { Selection } from './Selection'
+import { Layer } from '../document/Layer'
+import { Rect } from 'paintvec'
 
 export class App {
   @observable file = new File(new Document())
@@ -12,6 +14,15 @@ export class App {
   get layers () { return this.file.document.rootGroup.children }
   readonly scroll = new Scroll()
   readonly selection = new Selection(this.file.document)
+
+  constructor () {
+    // TODO: remove later
+    const layer = new Layer()
+    layer.shape = new RectShape()
+    layer.rect = Rect.fromWidthHeight(10, 20, 30, 40)
+    layer.name = 'Layer'
+    this.document.rootGroup.children.push(layer)
+  }
 }
 
 export const app = new App()
