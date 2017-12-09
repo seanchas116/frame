d959c0b58382d23b6702fd1ac670d5bdf5a667b6
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-09 17:48:48 +0900

    Add SVG

--- src/ui/drawarea/DrawArea.tsx
+++ src/ui/drawarea/DrawArea.tsx
@@ -1,8 +1,44 @@
 import * as React from 'react'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
+import { Rect } from 'paintvec'
 const styles = require('./DrawArea.css')
 
-export class DrawArea extends React.Component {
+interface ResizeObserver {
+  observe (e: Element): void
+  unobserve (e: Element): void
+}
+
+interface ResizeObserverStatic {
+  new (callback: () => void): ResizeObserver
+}
+declare var ResizeObserver: ResizeObserverStatic
+
+@observer export class DrawArea extends React.Component {
+  @observable clientRect = new Rect()
+
+  private element: HTMLElement
+  private resizeObserver: ResizeObserver
+
+  componentDidMount () {
+    this.handleResize()
+    this.resizeObserver = new ResizeObserver(this.handleResize)
+    this.resizeObserver.observe(this.element)
+  }
+
+  componentWillUnmount () {
+    this.resizeObserver.unobserve(this.element)
+  }
+
   render () {
-    return <div className={styles.DrawArea}>Hello</div>
+    const { width, height } = this.clientRect
+    return <div className={styles.DrawArea} ref={e => this.element = e!}>
+      <svg width={width} height={height} />
+    </div>
+  }
+
+  private handleResize = () => {
+    const clientRect = this.element.getBoundingClientRect()
+    this.clientRect = Rect.fromWidthHeight(clientRect.left, clientRect.top, clientRect.width, clientRect.height)
   }
 }
