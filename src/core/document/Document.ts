b5b69fc7174c51f01175aa5506c4a0f350948ff7
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-24 22:28:16 +0900

    Add History to Document

--- src/core/document/Document.ts
+++ src/core/document/Document.ts
@@ -1,15 +1,21 @@
 import { Layer } from './Layer'
 import { observable } from 'mobx'
 import { GroupShape } from './Shape'
+import { History } from './History'
 
 export class Document {
   @observable rootGroup = new Layer()
+  private readonly history = new History(this)
+
+  get undoStack () {
+    return this.history.undoStack
+  }
 
   constructor () {
     this.rootGroup.shape = new GroupShape()
   }
 
   commit (message: string) {
-    // TODO
+    this.history.commit(message)
   }
 }
