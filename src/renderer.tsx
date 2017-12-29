007c3e3c4c8ddd59f19780444f71cedb2e59cf09
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-29 16:15:29 +0900

    Load plugin

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import BasicActionsPlugin from './plugins/basic-actions'
 import './config'
 import { AppView } from './app/views/AppView'
+import { pluginRegistry } from './app/state/Plugin'
+
+pluginRegistry.load(new BasicActionsPlugin())
 
 ReactDOM.render(<AppView />, document.getElementById('root'))
