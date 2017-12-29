0f88fae381adbdcafc43014af32d64dab82e8d39
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-29 16:15:16 +0900

    Add defaultKey property to Action

--- src/app/state/Action.ts
+++ src/app/state/Action.ts
@@ -1,13 +1,15 @@
+import { ObservableMap } from '../../core/common/ObservableMap'
 
 export interface Action {
   id: string
   title: string
   enabled: boolean
+  defaultKey: string | undefined
   run (): void
 }
 
 export class ActionRegistry {
-  private actions = new Map<string, Action>()
+  private actions = new ObservableMap<string, Action>()
 
   add (action: Action) {
     this.actions.set(action.id, action)
