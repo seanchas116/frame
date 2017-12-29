e96b8f050856ab98e85e47620b0339a763415e8d
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-29 17:24:38 +0900

    Test File.open

--- src/app/state/File.test.ts
+++ src/app/state/File.test.ts
@@ -1,25 +1,29 @@
 import { assert } from 'chai'
 import * as tmp from 'tmp'
-import * as fs from 'fs'
 import { File } from './File'
 import { createDocument } from '../../core/document/test/Fixture'
 import { Document } from '../../core/document/Document'
+import { documentToData } from '../../core/format/v1/Serialize';
 
 describe('File', () => {
   describe('constructor', () => {
     it('creates empty file', () => {
       const file = new File(new Document())
-      assert.notOk(file.isModified)
+      assert.equal(file.isModified, false)
     })
   })
-  describe('#save', () => {
+  describe('#save/.open', () => {
     it('saves document to file', async () => {
       const file = new File(createDocument())
       file.path = tmp.tmpNameSync()
       file.document.commit('Add Layers')
       await file.save()
-      assert(fs.existsSync(file.path))
-      assert.notOk(file.isModified)
+      assert.equal(file.isModified, false)
+
+      const openedFile = await File.open(file.path)
+      assert.deepEqual(documentToData(openedFile.document), documentToData(file.document))
+      assert.equal(openedFile.isModified, false)
+      assert.equal(openedFile.path, file.path)
     })
   })
 })
