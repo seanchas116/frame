1a8a8381e9bc30ca263a7d183b450b237d55fb57
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-29 16:55:34 +0900

    Implement File

--- src/app/state/File.ts
+++ src/app/state/File.ts
@@ -1,9 +1,28 @@
 import { Document } from '../../core/document/Document'
 import { Format } from '../../core/format/Format'
 import { V1Format } from '../../core/format/v1/V1Format'
+import { observable, reaction } from 'mobx'
+import * as fs from 'fs'
 
 export class File {
   readonly format: Format = new V1Format()
+  @observable isModified = false
+  @observable path: string | undefined = undefined
+
   constructor (public readonly document: Document) {
+    reaction(() => document.undoStack.commandToUndo, () => this.isModified = true)
+  }
+
+  async save () {
+    if (!this.isModified) {
+      return
+    }
+    if (this.path) {
+      const data = await this.format.serialize(this.document)
+      fs.writeFileSync(this.path, data)
+      this.isModified = false
+    } else {
+      throw new Error('path is not specified')
+    }
   }
 }
