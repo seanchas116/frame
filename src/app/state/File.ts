ef351b5eca2111dd9b6a28065c6621cf4f758447
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-29 17:24:34 +0900

    Implement File.open

--- src/app/state/File.ts
+++ src/app/state/File.ts
@@ -1,11 +1,11 @@
 import { Document } from '../../core/document/Document'
-import { Format } from '../../core/format/Format'
 import { V1Format } from '../../core/format/v1/V1Format'
 import { observable, reaction } from 'mobx'
 import * as fs from 'fs'
 
+const fileFormat = new V1Format()
+
 export class File {
-  readonly format: Format = new V1Format()
   @observable path: string | undefined = undefined
   get isModified () { return this._isModified }
   @observable private _isModified = false
@@ -14,12 +14,20 @@ export class File {
     reaction(() => document.undoStack.commandToUndo, () => this._isModified = true)
   }
 
+  static async open (path: string) {
+    const data = fs.readFileSync(path)
+    const document = await fileFormat.deserialize(data)
+    const file = new File(document)
+    file.path = path
+    return file
+  }
+
   async save () {
     if (!this._isModified) {
       return
     }
     if (this.path) {
-      const data = await this.format.serialize(this.document)
+      const data = await fileFormat.serialize(this.document)
       fs.writeFileSync(this.path, data)
       this._isModified = false
     } else {
