f5a3284f383590319859850d82652999bcf5d371
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-30 18:57:37 +0900

    Open from URL hash

--- src/app/state/FileStore.ts
+++ src/app/state/FileStore.ts
@@ -1,13 +1,29 @@
 import * as Electron from 'electron'
+import * as querystring from 'querystring'
+import { observable } from 'mobx'
 import { File } from './File'
 import { Document } from '../../core/document/Document'
 
 export class FileStore {
-  readonly file = new File(new Document())
+  @observable file = new File(new Document())
   get document () { return this.file.document }
+
+  constructor () {
+    this.openFileFromHash()
+  }
 
   openInNewWindow (path?: string) {
     Electron.ipcRenderer.send('newWindow', path)
+  }
+
+  private async openFileFromHash () {
+    if (location.hash) {
+      const opts = querystring.parse(location.hash.slice(1))
+      if (opts.filePath) {
+        // TODO: handle error
+        this.file = await File.open(opts.filePath as string)
+      }
+    }
   }
 }
 
