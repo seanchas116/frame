a82e525c4dbb9c345e5fb84881a2466f9f7cd68e
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-30 18:52:35 +0900

    Add openInNewWindow

--- src/app/state/FileStore.ts
+++ src/app/state/FileStore.ts
@@ -1,9 +1,14 @@
+import * as Electron from 'electron'
 import { File } from './File'
 import { Document } from '../../core/document/Document'
 
 export class FileStore {
   readonly file = new File(new Document())
   get document () { return this.file.document }
+
+  openInNewWindow (path?: string) {
+    Electron.ipcRenderer.send('newWindow', path)
+  }
 }
 
 export const fileStore = new FileStore()
