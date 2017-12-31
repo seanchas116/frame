ebcb3218c40819ce32e6554f41f47b72896d2c7a
Ryohei Ikegami <iofg2100@gmail.com>
2017-12-31 15:42:06 +0900

    Load FileActionsExtension

--- src/config/extensions.ts
+++ src/config/extensions.ts
@@ -1,4 +1,6 @@
 import { extensionRegistry } from '../app/state/Extension'
 import EditActionsExtension from '../extensions/edit-actions'
+import FileActionsExtension from '../extensions/file-actions'
 
 extensionRegistry.add(new EditActionsExtension())
+extensionRegistry.add(new FileActionsExtension())
