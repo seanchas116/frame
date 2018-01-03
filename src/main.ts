15997ade77d9a25bfba684dd849f46f5cde4866b
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-03 18:10:55 +0900

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,68 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
+import * as Electron from 'electron'
 import * as qs from 'querystring'
 import * as transparentTitlebar from 'transparent-titlebar'
 
 const argv = require('minimist')(process.argv.slice(2))
-const contentBase = argv.devserver ? 'http://localhost:23000' : `file://${app.getAppPath()}/dist`
+const contentBase = argv.devserver ? 'http://localhost:23000' : `file://${Electron.app.getAppPath()}/dist`
 
-let mainWindow: BrowserWindow | undefined
-let testWindow: BrowserWindow | undefined
+class Window {
+  static instances = new Set<Window>()
 
-async function openWindow (filePath?: string) {
-  const win = mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 768,
-    show: false
-  })
-  win.setTitle('Frame')
-  transparentTitlebar.setup(win.getNativeWindowHandle())
-
-  const query = qs.stringify({ filePath })
-
-  win.loadURL(`${contentBase}/index.html#${query}`)
-  if (argv.devserver) {
-    win.webContents.openDevTools()
-  }
-
-  win.on('closed', () => {
-    mainWindow = undefined
-  })
-
-  win.on('ready-to-show', () => {
-    win.show()
-  })
-}
-
-function openTestWindow () {
-  const win = testWindow = new BrowserWindow({
-    width: 1200,
-    height: 768
-  })
-  win.loadURL(`${contentBase}/test.html`)
-  win.webContents.openDevTools()
-  win.on('closed', () => {
-    testWindow = undefined
-  })
-}
-
-app.commandLine.appendSwitch('enable-experimental-web-platform-features')
-
-app.on('ready', async () => {
-  if (argv.test) {
-    await openTestWindow()
-  } else {
-    ipcMain.on('newWindow', async (e: Electron.IpcMessageEvent, filePath?: string) => {
-      await openWindow(filePath)
+  constructor (public browserWindow: Electron.BrowserWindow) {
+    Window.instances.add(this)
+    browserWindow.on('closed', () => {
+      Window.instances.delete(this)
     })
-    await openWindow()
+  }
+}
+
+class DocumentWindow extends Window {
+
+  constructor (public filePath?: string) {
+    super(new Electron.BrowserWindow({
+      width: 1200,
+      height: 768,
+      show: false
+    }))
+    transparentTitlebar.setup(this.browserWindow.getNativeWindowHandle())
+
+    const query = qs.stringify({ filePath })
+
+    this.browserWindow.loadURL(`${contentBase}/index.html#${query}`)
+
+    if (argv.devserver) {
+      this.browserWindow.webContents.openDevTools()
+    }
+
+    this.browserWindow.on('ready-to-show', () => {
+      this.browserWindow.show()
+    })
+  }
+}
+
+class TestWindow extends Window {
+  constructor () {
+    super(new Electron.BrowserWindow({
+      width: 1200,
+      height: 768
+    }))
+    this.browserWindow.loadURL(`${contentBase}/test.html`)
+    this.browserWindow.webContents.openDevTools()
+  }
+}
+
+Electron.app.commandLine.appendSwitch('enable-experimental-web-platform-features')
+
+Electron.app.on('ready', () => {
+  if (argv.test) {
+    // tslint:disable-next-line:no-unused-expression
+    new TestWindow()
+  } else {
+    Electron.ipcMain.on('newWindow', async (e: Electron.IpcMessageEvent, filePath?: string) => {
+      // tslint:disable-next-line:no-unused-expression
+      new DocumentWindow(filePath)
+    })
+    // tslint:disable-next-line:no-unused-expression
+    new DocumentWindow()
   }
 })
