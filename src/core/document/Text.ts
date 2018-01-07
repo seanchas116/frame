e3e3d4d563b3ed0bad46690826db12e1d5b3191c
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-07 15:33:44 +0900

    Remove extra space

--- src/core/document/Text.ts
+++ src/core/document/Text.ts
@@ -3,7 +3,7 @@ import { HSVColor } from '../common/Color'
 
 export class Text {
   @observable fontFamily: string | undefined = undefined
-  @observable fontSize : number | undefined = undefined
+  @observable fontSize: number | undefined = undefined
   @observable fontWeight: number | undefined = undefined
   @observable color: HSVColor | undefined = undefined
   readonly children = observable<Text | string>([])
