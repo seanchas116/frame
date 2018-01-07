10776aa7150a995cc718a5f9eb6c8c87b7525567
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-07 15:51:16 +0900

    Handle text input

--- src/editor/views/TextEditor.tsx
+++ src/editor/views/TextEditor.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
+import { action } from 'mobx'
 import { Layer } from '../../core/document/Layer'
 const styles = require('./TextEditor.css')
 
 export class TextEdior extends React.Component<{layer: Layer}> {
+  element: HTMLDivElement
+
   render () {
     const { left, top, width, height } = this.props.layer.rect
     const style = {
@@ -11,6 +14,10 @@ export class TextEdior extends React.Component<{layer: Layer}> {
       width: width + 'px',
       height: height + 'px'
     }
-    return <div className={styles.TextEditor} style={style} contentEditable>Test</div>
+    return <div ref={e => this.element = e!} className={styles.TextEditor} style={style} onInput={this.handleInput} contentEditable={true}>Test</div>
+  }
+
+  @action private handleInput = () => {
+    console.log(this.element.innerText)
   }
 }
