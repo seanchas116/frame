69a31feee060d9e255e1e284e2dafbd84f593767
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-08 13:26:20 +0900

    Add inputs

--- src/app/views/Inspector.tsx
+++ src/app/views/Inspector.tsx
@@ -4,6 +4,20 @@ const styles = require('./Inspector.css')
 export class Inspector extends React.Component {
   render () {
     return <div className={styles.Inspector}>
+      <div className={styles.Header}>Fill</div>
+      <div className={styles.FillRow}>
+        <input type='checkbox' />
+        <input type='color' />
+      </div>
+      <div className={styles.Header}>Border</div>
+      <div className={styles.StrokeRow}>
+        <input type='checkbox' />
+        <input type='color' />
+        <select>
+          <option>Center</option>
+        </select>
+        <input type='number' />
+      </div>
     </div>
   }
 }
