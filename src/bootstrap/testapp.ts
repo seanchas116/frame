6ea33e4065ddb979d2f28a2dd544359669a38430
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-23 22:23:23 +0900

    Fix testapp

--- src/bootstrap/testapp.ts
+++ src/bootstrap/testapp.ts
@@ -1,2 +1,2 @@
-const requires = require.context('mocha-loader!./', true, /\.test\.ts$/)
+const requires = require.context('mocha-loader!../', true, /\.test\.ts$/)
 requires.keys().forEach(requires)
