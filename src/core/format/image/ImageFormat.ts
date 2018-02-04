a879d2bc9e9cd6debb7aff7b85b8598ca3ceb1ff
Ryohei Ikegami <iofg2100@gmail.com>
2018-02-04 20:35:27 +0900

    Implement ImageFormat

--- src/core/format/image/ImageFormat.ts
+++ src/core/format/image/ImageFormat.ts
@@ -1,5 +1,8 @@
 import { Format } from '../Format'
 import { Document } from '../../document/Document'
+import { Rect } from 'paintvec'
+import { ImageShape } from '../../document/Shape'
+import { Layer } from '../../document/Layer'
 
 export abstract class ImageFormat implements Format {
   abstract readonly extensions: ReadonlyArray<string>
@@ -12,6 +15,16 @@ export abstract class ImageFormat implements Format {
   }
 
   async deserialize (buffer: Buffer): Promise<Document> {
-    throw new Error('TODO')
+    const dataURL = `data:${this.mime};base64,${buffer.toString('base64')}`
+    const layer = new Layer()
+    const shape = new ImageShape()
+    await shape.loadDataURL(dataURL)
+    layer.shape = shape
+    layer.rect = Rect.fromWidthHeight(0, 0, shape.originalSize.width, shape.originalSize.height)
+    layer.name = 'Image'
+
+    const document = new Document()
+    document.rootGroup.children.replace([layer])
+    return document
   }
 }
