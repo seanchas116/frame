88d1dc29d12018b899509dee7420e80d1b035ef8
Ryohei Ikegami <iofg2100@gmail.com>
2018-02-04 20:31:48 +0900

    Add docs for Format

--- src/core/format/Format.ts
+++ src/core/format/Format.ts
@@ -3,10 +3,29 @@ import { observable } from 'mobx'
 import { Format } from './Format'
 
 export interface Format {
+  /**
+   * File extensions for the format
+   */
   readonly extensions: ReadonlyArray<string>
+
+  /**
+   * MIME type for the format
+   */
   readonly mime: string
+
+  /**
+   * macOS UTI for the format
+   * https://developer.apple.com/library/content/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html
+   */
   readonly uti: string
+
+  /**
+   * Whether the format is loaded as multiple layers
+   * False if the format represents non-layered image (PNG, JPG, ...)
+   * True if the format can have multiple layers (PSD, SVG, ...)
+   */
   readonly isLayered: boolean
+
   serialize (document: Document): Promise<Buffer>
   deserialize (buffer: Buffer): Promise<Document>
 }
