5d4bb7e0706010ed67528dff2c216f3d2766234a
Ryohei Ikegami <iofg2100@gmail.com>
2018-02-05 22:01:55 +0900

    Add Scoll#scale

--- src/core/document/Scroll.ts
+++ src/core/document/Scroll.ts
@@ -3,11 +3,19 @@ import { Vec2, Transform } from 'paintvec'
 
 export class Scroll {
   @observable translation = new Vec2()
+  @observable scale = 1
 
   @computed get viewportToDocument () {
-    return Transform.translate(this.translation)
+    return Transform.translate(this.translation).scale(new Vec2(1 / this.scale))
   }
   @computed get documentToViewport () {
-    return Transform.translate(this.translation.neg())
+    return Transform.scale(new Vec2(this.scale)).translate(this.translation.neg())
+  }
+
+  zoomIn () {
+    this.scale *= 2
+  }
+  zoomOut () {
+    this.scale /= 2
   }
 }
