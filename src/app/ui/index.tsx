42e80281cf41df9c3feaf88bdb1521513b5f27e8
Ryohei Ikegami <iofg2100@gmail.com>
2018-02-09 11:19:12 +0900

    Mount React on window load

--- src/app/ui/index.tsx
+++ src/app/ui/index.tsx
@@ -2,7 +2,9 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { AppView } from './AppView'
 
-ReactDOM.render(<AppView />, document.getElementById('root'))
+window.addEventListener('load', () => {
+  ReactDOM.render(<AppView />, document.getElementById('root'))
+})
 
 if (module.hot) {
   module.hot.accept()
