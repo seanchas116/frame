28dd01be1c83058379f4dd708b9def20b69b0b72
Ryohei Ikegami <iofg2100@gmail.com>
2018-02-11 09:57:35 +0900

    Fix hot reload

--- src/app/ui/index.tsx
+++ src/app/ui/index.tsx
@@ -2,9 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { AppView } from './AppView'
 
-window.addEventListener('load', () => {
-  ReactDOM.render(<AppView />, document.getElementById('root'))
-})
+ReactDOM.render(<AppView />, document.getElementById('root'))
 
 if (module.hot) {
   module.hot.accept()
