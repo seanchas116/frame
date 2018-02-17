cb406a9e27242aa10d9433e67d61ad90ec258924
Ryohei Ikegami <iofg2100@gmail.com>
2018-02-17 23:23:47 +0900

    Add styleForRange

--- src/core/document/Text.ts
+++ src/core/document/Text.ts
@@ -1,16 +1,20 @@
 import { observable } from 'mobx'
 import { HSVColor } from '../../lib/Color'
+import { sameOrNone } from '../../lib/sameOrNone'
 
 export interface TextRange {
   begin: number
   end: number
 }
 
-export interface TextSpan {
+export interface TextStyle {
   readonly family?: string
   readonly size: number
   readonly weight: number
   readonly color: HSVColor
+}
+
+export interface TextSpan extends TextStyle {
   readonly content: string
 }
 
@@ -26,5 +30,31 @@ export class Text {
 
   get isEmpty () {
     return this.spans.length === 0
+  }
+
+  spansWithRange (): [TextSpan, TextRange][] {
+    let pairs: [TextSpan, TextRange][] = []
+    let lastEnd = 0
+    for (const span of this.spans) {
+      let begin = lastEnd
+      let end = begin + span.content.length
+      pairs.push([span, { begin, end }])
+    }
+    return pairs
+  }
+
+  styleForRange (range: TextRange): Partial<TextStyle> {
+    const spansInRange: TextSpan[] = []
+    for (const [span, spanRange] of this.spansWithRange()) {
+      if (range.begin < spanRange.end && spanRange.begin < range.end) {
+        spansInRange.push(span)
+      }
+    }
+    return {
+      family: sameOrNone(spansInRange.map(s => s.family)),
+      size: sameOrNone(spansInRange.map(s => s.size)),
+      weight: sameOrNone(spansInRange.map(s => s.weight)),
+      color: sameOrNone(spansInRange.map(s => s.color), (c1, c2) => c1.equals(c2))
+    }
   }
 }
